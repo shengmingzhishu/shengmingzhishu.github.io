ad5670f8f17c2c59af6e69fb0992deec2f26c77c
lemon <liguiqiang@aliyun.com>
2023-04-10 02:14:20 +0800

    初始化 index-01.html     完成了单词点击，显示隐藏功能 index-02.html     完成了随处出现，单机排序功能 备注：     建议小于3个词的部分，直接显示正确答案，用户只关心3个字母以上的单词

--- LearnEnglish/utils/BookUtils.js
+++ LearnEnglish/utils/BookUtils.js
@@ -2,9 +2,10 @@
 
 function getBookEnum(){
     return {
-        BOOK50TO700Words: { bookNo: 1, bookName: '50句子7000词', path: 'wordlearn/phrase/50句子700词.js.js' },
-        LuoMaJiaRi: { bookNo: 2, bookName: '羅馬假日', path: 'wordlearn/phrase/罗马假日.js' },
-        ZaoAnYingYu: { bookNo: 3, bookName: '早安英语', path: 'wordlearn/phrase/早安英语.js' }
+        BOOK50TO700Words: { bookNo: 1, bookName: '50句子7000词', path: 'wordlearn/phrase/50句子700词.js' },
+        BOOK50Words: { bookNo: 2, bookName: '50短句', path: 'wordlearn/phrase/50短句.js' },
+        LuoMaJiaRi: { bookNo: 3, bookName: '羅馬假日', path: 'wordlearn/phrase/罗马假日.js' },
+        ZaoAnYingYu: { bookNo: 4, bookName: '早安英语', path: 'wordlearn/phrase/早安英语.js' }
     }
 }
 
@@ -13,8 +14,10 @@ function getIndexBook(num){
         case 1:
             return get50To7000BookStr()
         case 2:
-            return getLuoMaJiaRiBookStr()
+            return get50BookStr()
         case 3:
+            return getLuoMaJiaRiBookStr()
+        case 4:
             return getZaoAnYingYuBookStr()
         default:
             return get50To7000BookStr()
--- LearnEnglish/utils/FileUtils.js
+++ LearnEnglish/utils/FileUtils.js
@@ -1,3 +1,10 @@
+const colorList = [
+    "btn mr5 mb5 btn-primary"
+    , "btn mr5 mb5 btn-success"
+    , "btn mr5 mb5 btn-danger"
+    , "btn mr5 mb5 btn-warning"
+    , "btn mr5 mb5 btn-info"]
+
 /**
  * 段落拆分
  * */
@@ -10,18 +17,24 @@ function readParagraph(paragraph) {
     // 拼接短句数组元素
     let list = []
     for (let i = 0; i < len; i = i + 2) {
+        let EnglishWords = paragraphArr[i].split(' ');
+        let ChineseWords = paragraphArr[i + 1].split('');
         let obj = {
             EnglishPhrase: paragraphArr[i],
             ChinesePhrase: paragraphArr[i + 1],
             EnglishWordInfo: {
-                Words: paragraphArr[i].split(' '),
-                HideWords: paragraphArr[i].split(' ').map(item => toLine(item)),
-                ShowFlags:  paragraphArr[i].split(' ').map(item => true),// 默认显示单词
+                SuccessWords: EnglishWords.map(item => item),
+                Words: EnglishWords.map(item => item),
+                WordsClass: EnglishWords.map((item, index) => buttonColorClass(index)),
+                WordsEnabled: EnglishWords.map((item, index) => false),
+                HideWords: EnglishWords.map(item => toLine(item)),
+                ShowFlags: EnglishWords.map(item => true),// 默认显示单词
+                SelectedWords: EnglishWords.map(item => toLine(item))
             },
             ChineseWordInfo: {
-                Words: paragraphArr[i + 1].split(''),
-                HideWords: paragraphArr[i+1].split('').map(item => toLine(item)),
-                ShowFlags:  paragraphArr[i+1].split('').map(item => true),// 默认显示单词
+                Words: ChineseWords.map(item => item),
+                HideWords: ChineseWords.map(item => toLine(item)),
+                ShowFlags: ChineseWords.map(item => true),// 默认显示单词
             }
         }
         list.push(obj)
@@ -35,4 +48,9 @@ function toLine(str) {
         ret = ret + '_'
     }
     return ret
+}
+
+function buttonColorClass(index) {
+    let arr = colorList
+    return arr[parseInt(index % arr.length)]
 }
